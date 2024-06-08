a6346fd8896e2defcac72fb53ac4c1337ef917aa
cyRenShui <jqiown@163.com>
2024-06-08 15:54:06 +0800

    切换hash路由

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import {createRoot} from 'react-dom/client'
 import './style.css'
 import apolloClient from "./api/http"
 import {ApolloProvider} from "@apollo/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createHashRouter, RouterProvider} from "react-router-dom";
 import defaultProps from "./comp/Layout/_defaultProps";
 
 const container = document.getElementById('root')
 
 const root = createRoot(container!)
-const router = createBrowserRouter(defaultProps.routeDom, {
-    basename: import.meta.env.VITE_BUILDENV == 'vercel' ? '' : '/MemberManagement'
+const router = createHashRouter(defaultProps.routeDom, {
+    // basename: import.meta.env.VITE_BUILDENV == 'vercel' ? '' : '',
 });
 
 
