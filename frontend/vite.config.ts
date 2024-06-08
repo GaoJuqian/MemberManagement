2deafcf95b559557008a4b79b96f6faa01a134bc
cyRenShui <jqiown@163.com>
2024-06-08 10:03:43 +0800

    更改base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BUILDENV == 'vercel' ? '/MemberManagement/' : ''
+  base: process.env.BUILDENV == 'vercel' ? '' : '/MemberManagement/'
 })
